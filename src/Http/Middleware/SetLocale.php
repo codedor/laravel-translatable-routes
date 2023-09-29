b74b0dbb5143e090c9182fbb2ece25eb54d744e0
Jyrki De Neve <jyrki.deneve@gmail.com>
2023-09-29 13:29:30 +0300

    If no locale is found, continue to the next middleware, will mostly happen on splash pages

--- src/Http/Middleware/SetLocale.php
+++ src/Http/Middleware/SetLocale.php
@@ -4,7 +4,10 @@ namespace Codedor\TranslatableRoutes\Http\Middleware;
 
 use Closure;
 use Codedor\LocaleCollection\Facades\LocaleCollection;
+use Filament\Facades\Filament;
+use Filament\Panel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class SetLocale
 {
@@ -28,6 +31,10 @@ class SetLocale
             if (isset($snapshot['memo']['locale'])) {
                 $locale = $snapshot['memo']['locale'];
             }
+        }
+
+        if (! $locale) {
+            return $next($request);
         }
 
         LocaleCollection::setCurrent($locale, $request->root());
